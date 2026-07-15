94d817e77aa81941b7bf211bf4fe92e6ca522afc
Junaid Raza <148572941+junaidraza134@users.noreply.github.com>
2026-07-15 09:34:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.11%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sumodd=0;
+        int sumeven=0;
+        for(int i=1;i<=n;i++){
+            sumeven+=(2*i);
+            sumodd+=(2*i-1);
+        }
+        while(sumodd!=0){
+            int temp=sumodd;
+            sumodd=sumeven%sumodd;
+            sumeven=temp;
+        }
+        return sumeven;
+
+    }
+};
